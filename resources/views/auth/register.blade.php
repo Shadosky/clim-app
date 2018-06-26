32566369f65226ee8b1329c4554b4a318e57ff67
Kevin Giroux <kevin.giroux@stellatelecom.com>
2018-06-26 18:27:11 +0200

    registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.default')
-
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -60,4 +59,4 @@
         </div>
     </div>
 </div>
-@endsection
+@stop
