e24f5761b6896e48630ec7e6868c855fae58d43b
Kevin Giroux <kevin.giroux@stellatelecom.com>
2018-06-26 18:07:10 +0200

    database connection

---
+++ app/Models/Users.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevingiroux
+ * Date: 26/06/2018
+ * Time: 18:03
+ */
+namespace App\Models;
+
+
+class Users
+{
+    protected $connection = 'psql';
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'versions';
+
+    /**
+     * Fillable fields
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
