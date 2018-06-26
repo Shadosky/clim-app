83de79ea9b0ac7ed00f8480c37ba947a8b9d1760
Kevin Giroux <kevin.giroux@stellatelecom.com>
2018-06-26 17:47:39 +0200

    database connection

--- config/database.php
+++ config/database.php
@@ -1,5 +1,6 @@
 <?php
-
+// register database handling
+$dbopts = parse_url(getenv('DATABASE_URL'));
 return [
 
     /*
@@ -13,7 +14,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +57,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $dbopts["host"],
+            'port' => $dbopts["port"],
+            'database' => ltrim($dbopts["path"],'/'),
+            'username' => $dbopts["user"],
+            'password' => $dbopts["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
