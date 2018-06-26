5e6f31f575e63994d0a090b6ef3b00076db7a33f
Kevin Giroux <kevin.giroux@stellatelecom.com>
2018-06-27 00:45:11 +0200

    add content

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,5 +8,26 @@
 ?>
 @extends('layouts.default')
 @section('content')
-
+    <div class="row">
+        <div class="col-md-12">
+            <div class="col-md-6">
+                <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Clim ON</div>
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card text-white bg-warning mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Clim OFF</div>
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
