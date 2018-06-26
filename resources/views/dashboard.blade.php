a8a45ef4c4bd12a269cc589307ecdc6b7626a2ad
Kevin Giroux <kevin.giroux@stellatelecom.com>
2018-06-27 00:52:34 +0200

    restyle

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,23 +9,21 @@
 @extends('layouts.default')
 @section('content')
     <div class="row">
-        <div class="col-md-12">
-            <div class="col-md-6">
-                <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-                    <div class="card-header">Clim ON</div>
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                    </div>
+        <div class="col-md-6 text-center">
+            <div class="card text-white bg-info mb-3" style="max-width: 18rem; margin: auto;">
+                <div class="card-header">Clim ON</div>
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
                 </div>
             </div>
-            <div class="col-md-6">
-                <div class="card text-white bg-warning mb-3" style="max-width: 18rem;">
-                    <div class="card-header">Clim OFF</div>
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                    </div>
+        </div>
+        <div class="col-md-6 text-center">
+            <div class="card text-white bg-warning mb-3" style="max-width: 18rem; margin: auto;">
+                <div class="card-header">Clim OFF</div>
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
                 </div>
             </div>
         </div>
