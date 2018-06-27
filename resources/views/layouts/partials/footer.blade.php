0eb93616dc3be620bc0f0cf7a8e31d85a1187209
Kevin Giroux <kevin.giroux@stellatelecom.com>
2018-06-27 13:51:04 +0200

    add fontawesome

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -9,7 +9,7 @@
 <footer class="sticky-footer">
     <div class="container">
         <div class="text-center">
-            <small>Copyright © Skäah 2018</small>
+            <small>Made with <i class="fas fa-heart"></i> by Skäah</small>
         </div>
     </div>
 </footer>
