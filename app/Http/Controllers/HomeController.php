513a6c59d6dcc2ec6e91e2d462ad3716f2ea271c
Kevin Giroux <kevin.giroux@stellatelecom.com>
2018-06-27 17:48:14 +0200

    fetching votes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        dd(Auth::user()->id);
+        $user = Models\Users::find(Auth::user()->id);
+        dump($user);
+        dump($user->vote());
         return view('dashboard');
     }
 }
