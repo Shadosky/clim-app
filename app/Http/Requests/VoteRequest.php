11c3f9ec65b4f4b5ef0abfa6aed4c43043070ce1
Kevin Giroux <kevin.giroux@stellatelecom.com>
2018-06-28 00:56:52 +0200

    fix validator

--- app/Http/Requests/VoteRequest.php
+++ app/Http/Requests/VoteRequest.php
@@ -24,7 +24,7 @@ class VoteRequest extends FormRequest
     public function rules()
     {
         return [
-            'vote' => 'required|in(["a","b"])',
+            'vote' => 'required|in:a,b',
         ];
     }
 
